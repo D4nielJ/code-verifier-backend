3492e71603a316cfd66096051c054a900ef75e2a
D4nielJ <d4niel.djm@gmail.com>
2023-01-19 01:06:09 -0500

    Add Jest configuration

---
+++ __tests__/example.spec.ts
@@ -0,0 +1,5 @@
+describe("My first tests suite", () => {
+  it("should be true", () => {
+    expect(true).toBe(true);
+  });
+});
--- index.ts
+++ index.ts
@@ -13,8 +13,12 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to API: RESTful Express + TS + Swagger + Moongose");
 });
 
-// Execute APP and listen requests to PORT
+// Define HelloWorld route
+app.get("/hello", (req: Request, res: Response) => {
+  res.send("Welcome to GET route: Hello!");
+});
 
+// Execute APP and listen requests to PORT
 app.listen(port, () => {
   console.log(`EXPRESS SERVER: Running at http://localhost:${port}`);
 });
